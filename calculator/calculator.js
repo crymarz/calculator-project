b94c5d376b5197f31b9c7b53e31f99796dedfbab
crymarz <crymarz@milestechnologies.com>
2021-10-30 18:07:56 -0400

    first try

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -12,24 +12,62 @@ class Calculator {
     }
 
     // determine the current operation's label
-    getAction() {}
+    getAction() {
+        switch (this.operator) {
+            case "add":
+                return "adding"
+                break;
+            case "subtract":
+                return "subtracting"
+                break;
+            case "multiply":
+                return "multiplying"
+                break;
+            case "divide":
+                return "dividing"
+                break;
+        }
+    }
 
     // performa a calculation based on the currently selected operation
-    operate() {}
+    operate() {
+        if (this.operator === "add") {
+            return this.add()
+        } else if (this.operator === "subtract") {
+            return this.subtract()
+        } else if (this.operator == "multiply") {
+            return this.multiply()
+        } else if (this.operator === "divide") {
+            if (this.secondNumber === 0) {
+                this.attemptedDivideByZero = true
+            }
+            return this.divide()
+        }
+    }
 
     attemptedDivideByZero = false;
 
     // perform addition operation
-    add() {}
+    add() {
+        this.value = this.firstNumber + this.secondNumber
+    }
 
     // perform subtraction operation
-    subtract() {}
+    subtract() {
+        this.value = this.firstNumber - this.secondNumber
+    }
 
     // perform multiplication operation
-    multiply() {}
+    multiply() {
+        this.value = this.firstNumber * this.secondNumber
+    }
 
     // perform divide operation
-    divide() {}
+    divide() {
+        if (!this.attemptedDivideByZero) {
+            this.value = this.firstNumber / this.secondNumber
+        }
+    }
 }
 
 //DO NOT TOUCH THIS LINE OF CODE//
--- calculator/controller.js
+++ calculator/controller.js
@@ -1,20 +1,70 @@
 // setup variables and get a calculator instance
-function CreateCalculator() {}
+var calculator
+
+function CreateCalculator() {
+    calculator = new Calculator(parseInt(document.getElementById("inputNumberOne").value), parseInt(document.getElementById("inputNumberTwo").value), getOperator());
+}
 
 // perform a calculation when the operator button is clicked
-function calculate() {}
+function calculate() {
+    CreateCalculator();
+    calculator.operator = getOperator();
+    let result = calculator.operate();
+    updateResultText(calculator.value);
+    valid = true;
+    if (document.getElementById("inputNumberOne").value == "" || document.getElementById("inputNumberTwo").value == "") {
+        alert("Please enter a number");
+        valid = false;
+    }
+}
 
 /**
  * set the text in the result section of the UI
  * @param {*} value
  */
-function updateResultText(value) {}
+function updateResultText(value) {
+    let label = document.getElementById("result")
+    label.style = "color:black"
+    if (!calculator.attemptedDivideByZero) {
+        label.innerHTML = "The result of " + calculator.getAction() + " " + calculator.firstNumber + " and " + calculator.secondNumber + " is " + value
+    } else {
+        label.innerHTML = "You cannot divide by zero"
+        label.style = "color:red"
+    }
+}
 
 // should clear input text values and focus the first number input
-function clearValues() {}
+function clearValues() {
+    document.getElementById("inputNumberOne").value = null;
+    document.getElementById("inputNumberOne").focus();
+    document.getElementById("inputNumberTwo").value = null;
+    document.getElementById("result").innerHTML = ""
+    let radioButtons = document.getElementsByName("operation");
+    for (let i = 0; i < radioButtons.length; i++) {
+        radioButtons[i].checked = false;
+    }
+}
 
 /**
  * get the selected operator from the UI
  * @returns Operators
  */
-function getOperator() {}
+function getOperator() {
+    let radioButons = document.getElementsByName("operation")
+    let operator = "";
+    for (let i = 0; i < radioButons.length; i++) {
+        if (radioButons[i].checked) {
+            operator = radioButons[i].value;
+        }
+    }
+    return operator;
+}
+
+//Validate input fields
+function validateInputField() {
+    valid = true;
+    if (document.getElementById("inputNumberOne").value == "") {
+        alert("Please enter a number");
+        valid = false;
+    }
+}
